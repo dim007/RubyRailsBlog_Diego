730bb772c30a85ea43f81957d3c913d26558d020
dim007 <zeroxz1234@gmail.com>
2019-06-20 15:14:28 -0700

    Added functionality to the Card Compoenents.

--- app/javascript/components/ArticleCard.js
+++ app/javascript/components/ArticleCard.js
@@ -16,19 +16,20 @@ import {
 
 export default function ArticleCard(props) {
   return (
-    <Card style={{ maxWidth: "300px" }}>
+    <Card style={{ maxWidth: "800px" }}>
       <CardHeader>{props.title}</CardHeader>
       <CardImg src={props.img} />
       <CardBody>
         <CardTitle>{props.title}</CardTitle>
         <p>{props.body}</p>
-        <Button pill>Read more &rarr;</Button>
+        <Button pill href={'/articles/' + props.id}>Read more &rarr;</Button>
       </CardBody>
       <CardFooter>
         <ButtonToolbar>
           <ButtonGroup size="sm">
-            <Button outline theme="info">Edit</Button>
-            <Button outline theme="danger">Delete</Button>
+            <Button outline theme="info" href={'/articles/' + props.id}>Comment</Button>
+            <Button outline theme="warning" href={'/articles/' + props.id + '/edit'}>Edit</Button>
+            <Button outline data-confirm="Are you sure?" data-method='delete' href={'/articles/' + props.id} theme='danger'>Delete</Button>
           </ButtonGroup>
         </ButtonToolbar>
       </CardFooter>
