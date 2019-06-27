7813951dc60d205d876c937a5c874afc32a71e99
dim007 <zeroxz1234@gmail.com>
2019-06-26 18:42:03 -0700

    Updated toolbar with better one

---
+++ app/javascript/components/ArticleNavBar.js
@@ -0,0 +1,80 @@
+import React from "react";
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Collapse
+} from "shards-react";
+
+export default class ArticleNavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+    this.toggleNavbar = this.toggleNavbar.bind(this);
+
+    this.state = {
+      dropdownOpen: false,
+      collapseOpen: false
+    };
+  }
+
+  toggleDropdown() {
+    this.setState({
+      ...this.state,
+      ...{
+        dropdownOpen: !this.state.dropdownOpen
+      }
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      ...this.state,
+      ...{
+        collapseOpen: !this.state.collapseOpen
+      }
+    });
+  }
+
+  //className="ml-auto" this aligns to the right
+  render() {
+    return (
+      <Navbar type="dark" theme="primary" expand="md">
+        <NavbarBrand href="#">Diego Martinez</NavbarBrand>
+        <NavbarToggler onClick={this.toggleNavbar} />
+        
+        <Collapse open={this.state.collapseOpen} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink active href="#">
+                About
+              </NavLink>
+            </NavItem>
+            <Dropdown open={this.state.dropdownOpen} toggle={this.toggleDropdown}>
+              <DropdownToggle nav caret>Contact</DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem>Email</DropdownItem>
+                <DropdownItem>LinkedIn</DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </Nav>
+          <Nav navbar className="ml-auto"> 
+           <NavItem>
+              <NavLink href="#" active>
+                Sign In
+              </NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    );
+  }
+}
